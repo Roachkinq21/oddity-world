13e19e6d433935e2ce8a78f7d54aa2c4d4f85995
BrennonAry <thedoodlekidd@gmail.com>
2026-07-04 21:45:56 -0400

    Monster States

---
+++ Monsters/States/Chase.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class Chase : State
+{
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+}
---
+++ Monsters/States/Idle.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Idle : State
+{
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+
+}
