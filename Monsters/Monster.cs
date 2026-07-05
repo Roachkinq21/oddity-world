9e449471b84121cae01c87143eb60b893307bda4
BrennonAry <thedoodlekidd@gmail.com>
2026-07-04 21:21:28 -0400

    Monster State Machine

--- Monsters/Monster.cs
+++ Monsters/Monster.cs
@@ -1,18 +1,46 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class Monster : CharacterBody3D
 {
     
-    public float MovementSpeed {get;set;} = 2f;
+    public float MovementSpeed { get; set; } = 2f;
 
     public Area3D _monsterDetect;
 
+    public MonsterStateMachine MonsterStateMachine{get;set;}
+
+    public float Gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
 
+    public override void _Ready()
+    {
+        _monsterDetect = GetNode<Area3D>("MonsterDetect");
+        _monsterDetect.AreaEntered += _on_monster_detect_area_entered;
 
-    
-    public void _on_monster_detect_area_entered()
+        StateMachineAttribute = GetNode<MonsterStateMachine>("MonsterStateMachine");
+    }
+
+
+    public override void _PhysicsProcess(double delta)
+    {
+        float fDelta = (float)delta;
+
+        Vector3 velocity = Velocity;
+
+        if (!IsOnFloor())
+        {
+            velocity.Y -= Gravity * fDelta;
+        }
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
+
+
+    //Signal Events
+    private void _on_monster_detect_area_entered(Area3D area)
     {
         GD.Print("Player Spotted!");
     }
